16a7785d5e48a2828998cf1ba4e66707862e9702
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-07-08 12:22:45 +0000

    Separate old and new for Labels

--- plan_generator/src/enums.py
+++ plan_generator/src/enums.py
@@ -1,7 +1,7 @@
 from enum import Enum, auto
 
 
-class Labels(Enum):
+class LabelsOld(Enum):
     """Channel labels"""
 
     EXTERIOR_WALL_1 = 127
@@ -45,17 +45,17 @@ class LabelsNew(Enum):
     NUM_CLASSES = WALL
 
     CLASS_MAP = {
-        Labels.LIVING_ROOM.value: LIVING_ROOM,
-        Labels.MASTER_ROOM.value: MASTER_ROOM,
-        Labels.KITCHEN.value: KITCHEN,
-        Labels.BATHROOM.value: BATHROOM,
-        Labels.DINING_ROOM.value: DINING_ROOM,
-        Labels.CHILD_ROOM.value: CHILD_ROOM,
-        Labels.STUDY_ROOM.value: STUDY_ROOM,
-        Labels.SECOND_ROOM.value: SECOND_ROOM,
-        Labels.GUEST_ROOM.value: GUEST_ROOM,
-        Labels.BALCONY.value: BALCONY,
-        Labels.STORAGE.value: STORAGE,
+        LabelsOld.LIVING_ROOM.value: LIVING_ROOM,
+        LabelsOld.MASTER_ROOM.value: MASTER_ROOM,
+        LabelsOld.KITCHEN.value: KITCHEN,
+        LabelsOld.BATHROOM.value: BATHROOM,
+        LabelsOld.DINING_ROOM.value: DINING_ROOM,
+        LabelsOld.CHILD_ROOM.value: CHILD_ROOM,
+        LabelsOld.STUDY_ROOM.value: STUDY_ROOM,
+        LabelsOld.SECOND_ROOM.value: SECOND_ROOM,
+        LabelsOld.GUEST_ROOM.value: GUEST_ROOM,
+        LabelsOld.BALCONY.value: BALCONY,
+        LabelsOld.STORAGE.value: STORAGE,
     }
 
 
@@ -77,16 +77,30 @@ class Colors(Enum):
     BROWN = (165, 42, 42)
     GRAY = (128, 128, 128)
 
-    COLOR_MAP = {
-        Labels.LIVING_ROOM.value: GREEN,
-        Labels.MASTER_ROOM.value: BLUE,
-        Labels.KITCHEN.value: RED,
-        Labels.BATHROOM.value: PURPLE,
-        Labels.DINING_ROOM.value: YELLOW,
-        Labels.CHILD_ROOM.value: ORANGE,
-        Labels.STUDY_ROOM.value: MAGENTA,
-        Labels.SECOND_ROOM.value: BROWN,
-        Labels.GUEST_ROOM.value: BLUE,
-        Labels.BALCONY.value: PINK,
-        Labels.STORAGE.value: GRAY,
+    COLOR_MAP_OLD = {
+        LabelsOld.LIVING_ROOM.value: GREEN,
+        LabelsOld.MASTER_ROOM.value: BLUE,
+        LabelsOld.KITCHEN.value: RED,
+        LabelsOld.BATHROOM.value: PURPLE,
+        LabelsOld.DINING_ROOM.value: YELLOW,
+        LabelsOld.CHILD_ROOM.value: ORANGE,
+        LabelsOld.STUDY_ROOM.value: MAGENTA,
+        LabelsOld.SECOND_ROOM.value: BROWN,
+        LabelsOld.GUEST_ROOM.value: BLUE,
+        LabelsOld.BALCONY.value: PINK,
+        LabelsOld.STORAGE.value: GRAY,
+    }
+
+    COLOR_MAP_NEW = {
+        LabelsNew.LIVING_ROOM.value: GREEN,
+        LabelsNew.MASTER_ROOM.value: BLUE,
+        LabelsNew.KITCHEN.value: RED,
+        LabelsNew.BATHROOM.value: PURPLE,
+        LabelsNew.DINING_ROOM.value: YELLOW,
+        LabelsNew.CHILD_ROOM.value: ORANGE,
+        LabelsNew.STUDY_ROOM.value: MAGENTA,
+        LabelsNew.SECOND_ROOM.value: BROWN,
+        LabelsNew.GUEST_ROOM.value: BLUE,
+        LabelsNew.BALCONY.value: PINK,
+        LabelsNew.STORAGE.value: GRAY,
     }
