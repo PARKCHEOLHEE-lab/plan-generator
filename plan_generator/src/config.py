410afc9a6207dab50d2dc240f189e2e38fbe5b72
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-07-10 13:00:20 +0000

    Define modelconfigs

--- plan_generator/src/config.py
+++ plan_generator/src/config.py
@@ -5,6 +5,15 @@ class DataConfiguration:
     DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../data"))
     DATA_PATH = os.path.abspath(os.path.join(DATA_DIR, "plan"))
 
+    IMAGE_SIZE = 256
 
-class Configuration(DataConfiguration):
+
+class ModelConfiguration:
+    """Configuration for the model"""
+
+    STEPS = [64, 128, 256, 512, 1024]
+    EROSION_AND_DILATION_KERNEL_SIZE = (7, 7)
+
+
+class Configuration(DataConfiguration, ModelConfiguration):
     """Configuration for the plan generator"""
