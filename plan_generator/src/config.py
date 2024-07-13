809e12cd1d0eb30e6edf24f548b6cb1a61a5bcde
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-07-13 12:31:29 +0000

    Define model-related configs

--- plan_generator/src/config.py
+++ plan_generator/src/config.py
@@ -1,4 +1,7 @@
 import os
+import torch
+import random
+import numpy as np
 
 
 class DataConfiguration:
@@ -14,6 +17,36 @@ class ModelConfiguration:
     STEPS = [64, 128, 256, 512, 1024]
     EROSION_AND_DILATION_KERNEL_SIZE = (7, 7)
 
+    DEVICE = "cpu"
+    if torch.cuda.is_available():
+        DEVICE = "cuda"
+
+    SEED = 777
+
 
 class Configuration(DataConfiguration, ModelConfiguration):
     """Configuration for the plan generator"""
+
+    LOG_DIR = os.path.abspath(os.path.join(__file__, "../../runs"))
+
+    @staticmethod
+    def set_seed(seed: int = ModelConfiguration.SEED):
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        np.random.seed(seed)
+        random.seed(seed)
+
+        print("CUDA status")
+        print(f"  torch.cuda.is_available(): {torch.cuda.is_available()}")
+        print(f"  DEVICE: {Configuration.DEVICE} \n")
+
+        print("Seeds status:")
+        print(f"  Seeds set for torch        : {torch.initial_seed()}")
+        print(f"  Seeds set for torch on GPU : {torch.cuda.initial_seed()}")
+        print(f"  Seeds set for numpy        : {seed}")
+        print(f"  Seeds set for random       : {seed} \n")
+
+        Configuration.SEED = seed
