f12ae026cd5c842dd33e0d601e1d2ad37f016cd9
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-08-21 11:13:18 +0000

    `PlanGeneratorTrainer` wip

--- plan_generator/src/models.py
+++ plan_generator/src/models.py
@@ -1,10 +1,13 @@
 import os
 import sys
 import torch
+import datetime
 import pytorch_lightning as pl
 
 from torch import nn
-from typing import List
+from typing import List, Tuple
+from torch.utils.tensorboard import SummaryWriter
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 
@@ -13,8 +16,6 @@ if os.path.abspath(os.path.join(__file__, "../../../")) not in sys.path:
 
 from plan_generator.src.config import Configuration
 from plan_generator.src.data import PlanDataset, PlanDataLoader
-
-torch.set_float32_matmul_precision("medium")
 
 
 class UNetDecoder(nn.Module):
@@ -32,8 +33,8 @@ class UNetDecoder(nn.Module):
                     128 → 256 [64x64]  ─────────────────────────> 256 [64x64]
                         ↓                                               ▲
                         256 → 512 [32x32]  ───────────────> 512 [32x32]
-                            ↓                                    ▲
-                            512 → 1024 [16x16]  ───>  1024 [32x32]
+                            ↓                                      ▲
+                            512 → 1024 [16x16]  ─────>  1024 [32x32]
 
         legends:
             →     Conv2d(3x3)
@@ -53,8 +54,6 @@ class UNetDecoder(nn.Module):
         self.encoder_blocks = self._create_unet_encoder_blocks()
         self.upconv_blocks = self._create_unet_upconv_blocks()
         self.decoer_blocks = self._create_unet_decoder_blocks()
-
-        self.to(Configuration.DEVICE)
 
     def _encoder_block(self, in_channels: int, out_channels: int) -> nn.Sequential:
         return nn.Sequential(
@@ -176,8 +175,6 @@ class WallGenerator(nn.Module):
         self.encoder = MlpEncoder(self.size * self.size, self.size * 2, self.encoder_repeat)
         self.decoder = UNetDecoder(in_channels, out_channels, channels_step)
 
-        self.to(Configuration.DEVICE)
-
     def forward(self, floor: torch.Tensor) -> torch.Tensor:
         encoded = self.encoder(floor.reshape(-1, self.size * self.size)).reshape(-1, 1, self.size, self.size)
         decoded = self.decoder(encoded)
@@ -194,8 +191,6 @@ class RoomAllocator(nn.Module):
 
         self.encoder = MlpEncoder(self.size * self.size, self.size * 2, self.encoder_repeat)
         self.decoder = UNetDecoder(in_channels, out_channels, channels_step)
-
-        self.to(Configuration.DEVICE)
 
     def forward(self, walls: torch.Tensor) -> torch.Tensor:
         encoded = self.encoder(walls.reshape(-1, self.size * self.size)).reshape(-1, 1, self.size, self.size)
@@ -230,7 +225,9 @@ class DiceLoss(nn.Module):
         return loss.mean()
 
 
-class PlanGenerator(pl.LightningModule):
+class PlanGenerator(nn.Module):
+    """Floor plan generator consisting of the `WallGenerator` and `RoomAllocator`"""
+
     def __init__(self, configuration: Configuration):
         super().__init__()
 
@@ -252,10 +249,134 @@ class PlanGenerator(pl.LightningModule):
             encoder_repeat=self.configuration.ROOM_ALLOCATOR_REPEAT,
         )
 
+    def forward(self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor]:
+        floor_batch, walls_batch, _ = batch
+
+        generated_walls = self.wall_generator(floor_batch)
+        allocated_rooms = self.room_allocator(walls_batch)
+
+        return generated_walls, allocated_rooms
+
+    def infer(self):
+        return
+
+
+class PlanGeneratorTrainer:
+    """Trainer for the `PlanGenerator`"""
+
+    def __init__(self, plan_generator: PlanGenerator, existing_log_dir: str = None):
+        self.plan_generator = plan_generator
+        self.existing_log_dir = existing_log_dir
+
+        # Set summary writer
+        self.summary_writer = self._get_summary_writer(self.plan_generator.configuration, self.existing_log_dir)
+
+        # Set states if there is
+        self.states = self._get_states(self.summary_writer.log_dir)
+
+        # self.log_dir, self.states = self._set_summary_writer(self.plan_generator.configuration, existing_log_dir)
+
+        # Set optimizers
+        self.wall_generator_optimizer, self.room_allocator_optimizer = self._get_optimizers(
+            self.plan_generator.wall_generator, self.plan_generator.room_allocator, self.plan_generator.configuration
+        )
+
+        # Set schedulers
+        self.wall_generator_scheduler, self.room_allocator_scheduler = self._get_lr_schedulers(
+            self.wall_generator_optimizer, self.room_allocator_optimizer, self.plan_generator.configuration
+        )
+
+        # Set loss functions
+        self.wall_generator_loss_function, self.room_allocator_loss_function = self._get_loss_functions()
+
+    def _get_summary_writer(self, configuration: Configuration, existing_log_dir: str) -> SummaryWriter:
+        log_dir = os.path.join(configuration.LOG_DIR, datetime.datetime.now().strftime("%Y-%m-%d__%H-%M-%S"))
+
+        if existing_log_dir is not None:
+            log_dir = existing_log_dir
+
+        summary_writer = SummaryWriter(log_dir=log_dir)
+
+        return summary_writer
+
+    def _get_states(self, log_dir: str):
+        print(log_dir)  # FIXME: Use log_dir to load states dict of model trained
+        states = {}
+        return states
+
+    def _get_optimizers(
+        self, wall_generator: WallGenerator, room_allocator: RoomAllocator, configuration: Configuration
+    ) -> Tuple[torch.optim.Optimizer, torch.optim.Optimizer]:
+        wall_generator_optimizer = torch.optim.Adam(
+            wall_generator.parameters(), lr=configuration.WALL_GENERATOR_LEARNING_RATE
+        )
+
+        room_allocator_optimizer = torch.optim.Adam(
+            room_allocator.parameters(), lr=configuration.ROOM_ALLOCATOR_LEARNING_RATE
+        )
+
+        return wall_generator_optimizer, room_allocator_optimizer
+
+    def _get_lr_schedulers(
+        self,
+        wall_generator_optimizer: torch.optim.Optimizer,
+        room_allocator_optimizer: torch.optim.Optimizer,
+        configuration: Configuration,
+    ) -> Tuple[ReduceLROnPlateau, ReduceLROnPlateau]:
+        wall_generator_scheduler = ReduceLROnPlateau(
+            wall_generator_optimizer,
+            factor=configuration.WALL_GENERATOR_LEARNING_RATE_DECAY_FACTOR,
+            patience=configuration.WALL_GENERATOR_LEARNING_RATE_DECAY_PATIENCE,
+            verbose=True,
+        )
+
+        room_allocator_scheduler = ReduceLROnPlateau(
+            room_allocator_optimizer,
+            factor=configuration.ROOM_ALLOCATOR_LEARNING_RATE_DECAY_FACTOR,
+            patience=configuration.ROOM_ALLOCATOR_LEARNING_RATE_DECAY_PATIENCE,
+            verbose=True,
+        )
+
+        return wall_generator_scheduler, room_allocator_scheduler
+
+    def _get_loss_functions(self) -> Tuple[nn.BCELoss, nn.CrossEntropyLoss]:
+        wall_generator_loss_function = nn.BCELoss()
+        room_allocator_loss_function = nn.CrossEntropyLoss()
+
+        return wall_generator_loss_function, room_allocator_loss_function
+
+    def train(self):
+        pass
+
+
+class _PlanGenerator(pl.LightningModule):
+    def __init__(self, configuration: Configuration):
+        super().__init__()
+
+        self.configuration = configuration
+        self.save_hyperparameters(self.configuration.to_dict())
+        self.automatic_optimization = False
+        self.accumulation_steps = 8  # FIXME: migrate to configuration
+        self.current_step = 0  # FIXME: migrate to configuration
+
+        self.wall_generator = WallGenerator(
+            in_channels=self.configuration.WALL_GENERATOR_IN_CHANNELS,
+            out_channels=self.configuration.WALL_GENERATOR_OUT_CHANNELS,
+            size=self.configuration.IMAGE_SIZE,
+            channels_step=self.configuration.WALL_GENERATOR_CHANNELS_STEP,
+            encoder_repeat=self.configuration.WALL_GENERATOR_REPEAT,
+        )
+
+        self.room_allocator = RoomAllocator(
+            in_channels=self.configuration.ROOM_ALLOCATOR_IN_CHANNELS,
+            out_channels=self.configuration.ROOM_ALLOCATOR_OUT_CHANNELS,
+            size=self.configuration.IMAGE_SIZE,
+            channels_step=self.configuration.ROOM_ALLOCATOR_CHANNELS_STEP,
+            encoder_repeat=self.configuration.ROOM_ALLOCATOR_REPEAT,
+        )
+
         self.wall_generator_loss_function = nn.BCELoss()
         self.room_allocator_loss_function = nn.CrossEntropyLoss()
-
-        self.automatic_optimization = False
 
     def forward(self, floor: torch.Tensor, walls: torch.Tensor):
         generated_walls = self.wall_generator(floor)
@@ -264,38 +385,43 @@ class PlanGenerator(pl.LightningModule):
         return generated_walls, allocated_rooms
 
     def training_step(self, batch, _):
-        wall_generator_optimizer, room_allocator_optimizer = self.optimizers()
-        wall_generator_optimizer.zero_grad()
-        room_allocator_optimizer.zero_grad()
+        opt1, opt2 = self.optimizers()
 
         floor_batch, walls_batch, rooms_batch = batch
 
-        generated_walls, allocated_rooms = self(floor_batch, walls_batch)
-
+        # Wall Generator
+        generated_walls = self.wall_generator(floor_batch)
         masked_generated_walls = generated_walls.clone()
         masked_generated_walls[floor_batch == 0] = 0
+        wall_generator_loss = self.wall_generator_loss_function(masked_generated_walls, walls_batch)
 
+        # Room Allocator
+        allocated_rooms = self.room_allocator(walls_batch)
         masked_allocated_rooms = allocated_rooms.clone()
         masked_allocated_rooms[floor_batch.expand_as(allocated_rooms) == 0] = 0
-
-        wall_generator_loss = self.wall_generator_loss_function(masked_generated_walls, walls_batch)
         room_allocator_loss = self.room_allocator_loss_function(masked_allocated_rooms, rooms_batch.squeeze(1))
 
-        wall_generator_loss.backward()
-        room_allocator_loss.backward()
-
-        wall_generator_optimizer.step()
-        room_allocator_optimizer.step()
-
+        # 손실 로깅
         self.log("train_wall_generator_loss", wall_generator_loss, prog_bar=True, on_step=True, on_epoch=True)
         self.log("train_room_allocator_loss", room_allocator_loss, prog_bar=True, on_step=True, on_epoch=True)
         self.log(
             "train_total_loss", wall_generator_loss + room_allocator_loss, prog_bar=True, on_step=True, on_epoch=True
         )
 
+        # 수동으로 그래디언트 누적 및 최적화 수행
+        self.manual_backward(wall_generator_loss / self.accumulation_steps)
+        self.manual_backward(room_allocator_loss / self.accumulation_steps)
+
+        self.current_step += 1
+        if self.current_step % self.accumulation_steps == 0:
+            opt1.step()
+            opt2.step()
+            opt1.zero_grad()
+            opt2.zero_grad()
+
         return {
-            "wall_generator_loss": wall_generator_loss,
-            "room_allocator_loss": room_allocator_loss,
+            "train_wall_loss": wall_generator_loss,
+            "train_room_loss": room_allocator_loss,
             "train_total_loss": wall_generator_loss + room_allocator_loss,
         }
 
@@ -354,13 +480,21 @@ class PlanGenerator(pl.LightningModule):
         return [
             {
                 "optimizer": wall_generator_optimizer,
-                "lr_scheduler": wall_generator_scheduler,
-                "monitor": "val_wall_generator_loss",
+                "lr_scheduler": {
+                    "scheduler": wall_generator_scheduler,
+                    "monitor": "val_wall_generator_loss",
+                    "interval": "epoch",
+                    "frequency": 1,
+                },
             },
             {
                 "optimizer": room_allocator_optimizer,
-                "lr_scheduler": room_allocator_scheduler,
-                "monitor": "val_room_allocator_loss",
+                "lr_scheduler": {
+                    "scheduler": room_allocator_scheduler,
+                    "monitor": "val_room_allocator_loss",
+                    "interval": "epoch",
+                    "frequency": 1,
+                },
             },
         ]
 
@@ -377,7 +511,7 @@ if __name__ == "__main__":
 
     def train():
         config = Configuration()
-        model = PlanGenerator(config)
+        model = _PlanGenerator(config)
 
         # Create data loaders
         plan_dataset = PlanDataset()
@@ -402,11 +536,16 @@ if __name__ == "__main__":
             accelerator="auto",
             num_nodes=1,
             num_sanity_val_steps=1,
-            devices=torch.cuda.device_count(),
-            accumulate_grad_batches=8,
+            # devices=torch.cuda.device_count(),
+            devices=1,
+            # strategy="ddp",
         )
+
+        # FIXME: Use single gpu and accumulative gradient
 
         # Fit the model
         trainer.fit(model, plan_dataloader.train_loader, plan_dataloader.validation_loader)
 
-    train()
+    summary_writer = SummaryWriter(Configuration.LOG_DIR)
+
+    # train()
