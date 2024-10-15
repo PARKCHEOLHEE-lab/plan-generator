de16544b7535c48fa70b0ff9e5b62ffb1fe50c43
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2024-10-15 13:27:47 +0000

    remove caching

--- plan_generator/src/data.py
+++ plan_generator/src/data.py
@@ -4,7 +4,6 @@ import cv2
 import random
 import pickle
 import torch
-import functools
 import numpy as np
 import multiprocessing
 
@@ -139,10 +138,6 @@ class PlanDataset(Dataset):
 
         torch.multiprocessing.set_start_method("spawn", force=True)
 
-    @functools.lru_cache(maxsize=len(os.listdir(Configuration.DATA_SAVE_DIR)))
-    def _load_item(self, index):
-        return torch.load(self.dataset_paths[index])
-
     def __len__(self) -> int:
         return len(self.dataset_paths)
 
@@ -161,7 +156,7 @@ class PlanDataset(Dataset):
             3: 270 degree
         """
 
-        floor, walls, rooms = self._load_item(index)
+        floor, walls, rooms = torch.load(self.dataset_paths[index])
 
         floor = floor.unsqueeze(0)
         walls = walls.unsqueeze(0)
